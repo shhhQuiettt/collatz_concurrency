5c82339c63e99778eabf422984e9b0dcc2540df5
shhhQuiettt <krzysztof.skro@gmail.com>
2023-05-30 23:19:36 +0200

    Delete unused code

--- lib/collatzUtils.c
+++ lib/collatzUtils.c
@@ -17,27 +17,3 @@ uint64_t no_of_colnatz_steps(natural_number x) {
   }
   return steps;
 }
-
-void min_numbers_for_step(uint64_t s, volatile uint64_t *result) {
-  if (s < 11) {
-    perror("Cannot find numbers for less than 11 steps.");
-    return;
-  }
-  uint8_t min_counter = 0;
-  const uint64_t threshold = 1e10;
-  natural_number candidate = 1;
-
-  while (min_counter < 5) {
-    if (threshold < candidate) {
-      printf("No solution found for %ld, with threshold: %ld\n", s, threshold);
-      break;
-    }
-
-    if (no_of_colnatz_steps(candidate) == s) {
-      *(result + min_counter) = candidate;
-      ++min_counter;
-    }
-
-    ++candidate;
-  }
-}
--- lib/collatzUtils.h
+++ lib/collatzUtils.h
@@ -8,6 +8,3 @@
 typedef uint64_t natural_number;
 
 uint64_t no_of_colnatz_steps(natural_number x);
-
-void min_numbers_for_step(uint64_t s, volatile uint64_t* result);
-
